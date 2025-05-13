c50e705ef6bf7ff531ab1b1b3b7332349700344d
Dudda-soares <eduarda.soaresdsantos@gmail.com>
2025-05-13 15:58:16 -0300

    incluindo imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+
 function styles(){
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'})) //styles comprimido 
         .pipe(gulp.dest('./dist/css'));
 }
+
+
+
+
 
 exports.default = styles;
 exports.watch = function(){
