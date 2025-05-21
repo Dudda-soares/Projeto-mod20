849502bc6ba4b5bf8aaf2d64289256296103ca77
Dudda-soares <eduarda.soaresdsantos@gmail.com>
2025-05-21 15:40:12 -0300

    ajuste gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
 
 
 function styles(){
@@ -7,12 +8,18 @@ function styles(){
         .pipe(sass({outputStyle: 'compressed'})) //styles comprimido 
         .pipe(gulp.dest('./dist/css'));
 }
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 
 
 
-
-exports.default = styles;
+exports.default = gulp.parallel(styles, scripts);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss',gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js',gulp.parallel(scripts))
+
 }
